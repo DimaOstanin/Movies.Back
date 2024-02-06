621d7124e1020072cf1ddde119ae60461d2e7ca5
DimaOstanin <xnadab@gmail.com>
2024-02-06 13:48:31 +0200

    review ready, post method work

--- movies/src/main/java/dev/dima/movies/Review.java
+++ movies/src/main/java/dev/dima/movies/Review.java
@@ -3,16 +3,24 @@ package dev.dima.movies;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Review {
-    @Id
-    private Object id;
-    private String body;
+import java.time.LocalDateTime;
 
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
+        this.body = body;
+        this.created = created;
+        this.updated = updated;
+    }
 }
